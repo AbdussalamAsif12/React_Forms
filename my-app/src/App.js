6b6b1eb3b60e8eda29d67390668009bf94fd242a
Abdussalam <abdussalam4@gamil.com>
2023-12-26 02:49:17 +0500

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import LoginSignup from './Components/LoginSignup/LoginSignup'
+const App = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  )
+}
+
+export default App
